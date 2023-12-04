8d931393ac4ad50105806c8e93e8ff548aed2b54
Aidan Stambaugh <astambaugh2627@floridapoly.edu>
2023-12-04 12:17:35 -0500

    update

--- model.py
+++ model.py
@@ -1,7 +1,16 @@
+from tkinter import filedialog
+
 class Model:
-    def process_audio_file(self, file_path):
-        # Placeholder for processing the audio file and extracting relevant data
-        # You should implement this function based on your specific requirements
-        # You might use libraries like LibROSA or other audio processing tools
-        # Return a dictionary containing processed data (e.g., rt60, waveform, additional_data)
-        return {'rt60': 0.8, 'waveform': [0.1, 0.3, 0.5, 0.8, 1.0], 'additional_data': [0.2, 0.4, 0.6, 0.9, 1.2]}
+    def __init__(self, filename):
+        self.filename = filename
+        
+        @property
+        def filename(self):
+            return self.__filename
+
+        @filename.setter
+        def filename(self, value):
+            filepath =
+
+
+
