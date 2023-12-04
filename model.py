9dbb58bcdc881907993589c3d67c76ea73da788c
Aidan Stambaugh <astambaugh2627@floridapoly.edu>
2023-12-04 16:07:49 -0500

    test

--- model.py
+++ model.py
@@ -1,34 +1,58 @@
-from tkinter import filedialog
 import soundfile as sf
 import numpy as np
+import os
+from pydub import AudioSegment
 
 
 class Model:
-    def __init__(self):
-        self.filepath = None
+    def __init__(self, file_path):
+        self.file_path = file_path
         self.data = None
         self.sample_rate = None
         self.mono_data = None
 
-        def open_file(self):
-            filepath = filedialog.askopenfilename(title="Please select a .wav file.",
-                                                  filetypes=(("WAV files", "*.wav"), ("All files", "*.*")))
-            return filepath
+    def load_audio(self):
+        if not os.path.splitext(self.file_path)[1].lower() == '.wav':
+            # Converts the file to wav
+            src = self.file_path
+            dst = os.path.splitext(self.file_path)[0] + '.wav'
 
-        def read_wav_file(self):
-            if self.filepath:
-                self.data, self.sample_rate = sf.read(self.filepath)
+            sound = AudioSegment.from_mp3(src)
+            sound.export(dst, format="wav")
 
-                if len(self.data.shape) > 1:
-                    self.mono_data = np.mean(self.data, axis=1)
+            # Call the Model to process the selected file
+            time, waveform = self.process_audio_file(dst)
 
-                else:
-                    self.mono_data = self.data
+            return time, waveform
 
-                duration = len(self.mono_data) / self.sample_rate
-                print(f"Duration: {duration:.2f} seconds")
+            # Notify the View to update UI elements
+            self.view.update_filename_label(os.path.basename(dst))
+            self.view.update_rt60_label(sample_data['rt60'])
+            self.view.update_waveform_plot(sample_data['waveform'])
+            self.view.update_additional_plot(sample_data['additional_data'])
 
-        def run(self):
+        else:
+            # Call the Model to process the selected file
+            time, waveform = self.process_audio_file(self.file_path)
 
-            self.filepath = self.open_file()
-            self.read_wav_file()
+            return time, waveform
+
+            # Notify the View to update UI elements
+            self.view.update_filename_label(os.path.basename(self.file_path))
+            self.view.update_rt60_label(sample_data['rt60'])
+            self.view.update_waveform_plot(sample_data['waveform'])
+            self.view.update_additional_plot(sample_data['additional_data'])
+
+    def process_audio_file(self, filepath):
+        if filepath:
+            self.data, self.sample_rate = sf.read(filepath)
+
+            if len(self.data.shape) > 1:
+                self.mono_data = np.mean(self.data, axis=1)
+
+            else:
+                self.mono_data = self.data
+
+            duration = len(self.mono_data) / self.sample_rate
+
+            return duration, self.mono_data
