94edd8106a83f4bd57c301445f5e4846393e1c47
Fabian Rosario <fabianraul1210@gmail.com>
2023-12-07 22:16:19 -0500

    Working on making the RT60 value appear

--- view.py
+++ view.py
@@ -15,7 +15,7 @@ class View(ttk.Frame):
         # Create and place widgets
         # Load Button
         self.load_button = tk.Button(self, text="Load Audio", command=self.load_audio)
-        self.load_button.grid(row=1, column=1, pady=10)
+        self.load_button.grid(row=0, column=0, pady=10)
 
         self.graph_functions = [
             self.plot_waveform,
@@ -28,32 +28,36 @@ class View(ttk.Frame):
 
         self.figure, self.ax = plt.subplots()
         self.canvas = FigureCanvasTkAgg(self.figure, master=self)
-        self.canvas.get_tk_widget().grid(row=0, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+        self.canvas.get_tk_widget().grid(row=2, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
         self.canvas.draw()
 
         # Button to change graph
         self.change_graph_button = tk.Button(self, text="Switch Graph", command=self.change_graph)
-        self.change_graph_button.grid(row=2, column=1, pady=10)
+        self.change_graph_button.grid(row=3, column=0, pady=10)
 
         # Button to combine frequencies
         self.combine_button = tk.Button(self, text="Combine Frequencies", command=self.combine_frequencies)
-        self.combine_button.grid(row=3, column=1, pady=10)
+        self.combine_button.grid(row=3, column=0, pady=10, sticky=tk.E)
 
         # Filename Label
         self.filename_label = tk.Label(self, text="")
-        self.filename_label.grid(row=10, column=1)
+        self.filename_label.grid(row=1, column=0)
 
         # Display RT60
         self.rt60_label = tk.Label(self, text="")
-        self.rt60_label.grid(row=10, column=1, pady=10)
+        self.rt60_label.grid(row=4, column=0, pady=10)
 
         # Highest freq
         self.highest_res_freq_label = tk.Label(self, text="")
-        self.highest_res_freq_label.grid(row=17, column=1, pady=10)
+        self.highest_res_freq_label.grid(row=5, column=0, pady=10)
 
         # Total time of audio file
         self.time_label = tk.Label(self, text="")
-        self.time_label.grid(row=18, column=1, pady=5)
+        self.time_label.grid(row=6, column=0, pady=5)
+
+        self.spectrum = None
+        self.freqs = None
+        self.t = None
 
         # set the controller
         self.controller = None
@@ -62,7 +66,7 @@ class View(ttk.Frame):
         self.ax.clear()
         self.figure, self.ax = plt.subplots()
         self.canvas = FigureCanvasTkAgg(self.figure, master=self)
-        self.canvas.get_tk_widget().grid(row=0, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+        self.canvas.get_tk_widget().grid(row=2, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
         self.canvas.draw()
 
     def set_controller(self, controller):
@@ -97,10 +101,6 @@ class View(ttk.Frame):
     def update_filename_label(self, filename):
         # Update Filename Label
         self.filename_label.config(text=f"File: {filename}")
-
-    def update_rt60_label(self, rt60_value):
-        # Update RT60 label
-        self.rt60_label.config(text=f"RT60 Value: {rt60_value} seconds")
 
     def display_highest_freq(self, highest_res_freq):
         # Display the highest resonance frequency on the GUI
@@ -162,6 +162,7 @@ class View(ttk.Frame):
 
         # Call the combined RT60 graph function
         self.plot_combined_rt60()
+        self.update_rt60_label(self.spectrum, self.freqs, self.t)
 
         # Redraw the canvas with the new graph
         self.canvas.draw()
@@ -249,7 +250,7 @@ class View(ttk.Frame):
 
         # Plot spectrogram on the GUI
         sample_rate, data = wavfile.read(filepath)
-        spectrum, freqs, t, im = self.ax.specgram(data, Fs=sample_rate, NFFT=1024,
+        self.spectrum, self.freqs, self.t, im = self.ax.specgram(data, Fs=sample_rate, NFFT=1024,
                                                   cmap=plt.get_cmap('autumn_r'))
         cbar = plt.colorbar(im, ax=self.ax)
         cbar.set_label('Intensity (dB)')
@@ -257,3 +258,54 @@ class View(ttk.Frame):
         self.ax.set_ylabel('Frequency (Hz)')
         self.ax.set_title('Spectrogram')
         self.canvas.draw()
+
+    def update_rt60_label(self, spectrum, freqs, t):
+        def find_target_frequency(freqs):
+            for x in freqs:
+                if x > 1000:
+                    break
+            return x
+
+        def frequency_check(spectrum, freqs):
+            target_frequency = find_target_frequency(freqs)
+            index_of_frequency = np.where(freqs == target_frequency)[0][0]
+            # find sound data for a particular frequency
+            data_for_frequency = spectrum[index_of_frequency]
+            # change a digital signal for values in decibels
+            data_in_db_fun = 10 * np.log10(data_for_frequency)
+            return data_in_db_fun
+
+        data_in_db = frequency_check(spectrum, freqs)
+
+        plt.figure(2)
+        plt.plot(t, data_in_db, linewidth=1, alpha=0.7, color='#004bc6')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Power (dB)')
+
+        index_of_max = np.argmax(data_in_db)
+        value_of_max = data_in_db[index_of_max]
+
+        plt.plot(t[index_of_max], data_in_db[index_of_max], 'go')
+
+        sliced_array = data_in_db[index_of_max:]
+        value_of_max_less_5 = value_of_max - 5
+
+        def find_nearest_value(array, value):
+            array = np.asarray(array)
+            idx = (np.abs(array - value)).argmin()
+            return array[idx]
+
+        value_of_max_less_5 = find_nearest_value(sliced_array, value_of_max_less_5)
+        index_of_max_less_5 = np.where(data_in_db == value_of_max_less_5)[0]
+        plt.plot(t[index_of_max_less_5], data_in_db[index_of_max_less_5], 'yo')
+
+        value_of_max_less_25 = value_of_max - 25
+        value_of_max_less_25 = find_nearest_value(sliced_array, value_of_max_less_25)
+        index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)[0]
+        plt.plot(t[index_of_max_less_25], data_in_db[index_of_max_less_25], 'ro')
+
+        rt20 = (t[index_of_max_less_5] - t[index_of_max_less_25])[0]
+        rt60 = 3 * rt20
+
+        target_frequency = find_target_frequency(freqs)
+        print(f'The RT60 reverb time at freq {int(target_frequency)}Hz is {round(abs(rt60), 2)} seconds')
